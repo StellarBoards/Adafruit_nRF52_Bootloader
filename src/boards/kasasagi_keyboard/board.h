3cbc9e278cfb7df41fa008bb32ab3e7144aaf03f
FT256 <80577890+FT256@users.noreply.github.com>
2025-02-20 11:44:28 +0500

    Update board.h

--- src/boards/kasasagi_keyboard/board.h
+++ src/boards/kasasagi_keyboard/board.h
@@ -1,4 +1,3 @@
-#include <stdint.h>
 /*
  * The MIT License (MIT)
  *
@@ -57,9 +56,9 @@
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
-#define USB_DESC_VID           ((uint32_t)0xF256)
-#define USB_DESC_UF2_PID       ((uint32_t)0x01B0)
-#define USB_DESC_CDC_ONLY_PID  ((uint32_t)0x01B0)
+#define USB_DESC_VID           0x0256
+#define USB_DESC_UF2_PID       0x01B0
+#define USB_DESC_CDC_ONLY_PID  0x01B0
 
 #define UF2_PRODUCT_NAME  "Kasasagi"
 #define UF2_VOLUME_LABEL  "KASASAGI"
